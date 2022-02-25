8ec3df23d3cadd87862c127a175d0ce5b507743f
moudroff <moudroff>
2022-02-25 17:04:46 +0300

    Sqlite

---
+++ SqliteApp/SqliteApp/SqliteApp/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using SqliteApp.Models;
+using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SqliteApp
+{
+    public partial class App : Application
+    {
+        public const string DATABASE_NAME = "friends.db";
+        internal static FriendRepository database;
+        internal static FriendRepository Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new FriendRepository(
+                        Path.Combine(
+                            Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), DATABASE_NAME));
+                }
+                return database;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ SqliteApp/SqliteApp/SqliteApp/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using SqliteApp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SqliteApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            //friendsList.ItemsSource = App.Database.GetItems();
+        }
+
+        protected override void OnAppearing()
+        {
+            friendsList.ItemsSource = App.Database.GetItems();
+            base.OnAppearing();
+        }
+        // обработка нажатия элемента в списке
+        private async void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            Friend selectedFriend = (Friend)e.SelectedItem;
+            FriendPage friendPage = new FriendPage();
+            friendPage.BindingContext = selectedFriend;
+            await Navigation.PushAsync(friendPage);
+        }
+        // обработка нажатия кнопки добавления
+        private async void CreateFriend(object sender, EventArgs e)
+        {
+            Friend friend = new Friend();
+            FriendPage friendPage = new FriendPage();
+            friendPage.BindingContext = friend;
+            await Navigation.PushAsync(friendPage);
+        }
+    }
+}
