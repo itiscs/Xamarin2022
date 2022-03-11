0b37759e93c049722fb53749103f64ad4b6a5722
moudroff <moudroff>
2022-03-11 17:00:39 +0300

    Local Files

---
+++ AppFiles/AppFiles/AppFiles/MainPage.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace AppFiles
+{
+    public partial class MainPage : ContentPage
+    {
+        string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            UpdateFileList();
+            string name = Preferences.Get("name", "не установлено");
+            nameLabel.Text = name;
+        }
+        // сохранение текста в файл
+        async void Save(object sender, EventArgs args)
+        {
+            string filename = fileNameEntry.Text;
+            if (String.IsNullOrEmpty(filename)) return;
+            // если файл существует
+            if (File.Exists(Path.Combine(folderPath, filename)))
+            {
+                // запрашиваем разрешение на перезапись
+                bool isRewrited = await DisplayAlert("Подтверждение", "Файл уже существует, перезаписать его?", "Да", "Нет");
+                if (isRewrited == false) return;
+            }
+            // перезаписываем файл
+            File.WriteAllText(Path.Combine(folderPath, filename), textEditor.Text);
+            // обновляем список файлов
+            UpdateFileList();
+        }
+        void FileSelect(object sender, SelectedItemChangedEventArgs args)
+        {
+            if (args.SelectedItem == null) return;
+            // получаем выделенный элемент
+            string filename = (string)args.SelectedItem;
+            // загружем текст в текстовое поле
+            textEditor.Text = File.ReadAllText(Path.Combine(folderPath, (string)args.SelectedItem));
+            // устанавливаем название файла
+            fileNameEntry.Text = filename;
+            // снимаем выделение
+            filesList.SelectedItem = null;
+
+        }
+        void Delete(object sender, EventArgs args)
+        {
+            // получаем имя файла
+            string filename = (string)((MenuItem)sender).BindingContext;
+            // удаляем файл из списка
+            File.Delete(Path.Combine(folderPath, filename));
+            // обновляем список файлов
+            UpdateFileList();
+        }
+        // обновление списка файлов
+        void UpdateFileList()
+        {
+            // получаем все файлы
+            filesList.ItemsSource = Directory.GetFiles(folderPath).Select(f => Path.GetFileName(f));
+            // снимаем выделение
+            filesList.SelectedItem = null;
+        }
+
+        private void OnClick(object sender, EventArgs e)
+        {
+            string value = nameBox.Text;
+            nameLabel.Text = value;
+            Preferences.Set("name", value);
+        }
+    }
+
+}
